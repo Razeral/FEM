2358a915e1033700e3f64efa08033e2a1a16a9e0
Razeral <ng.shangru@gmail.com>
2018-05-06 18:23:20 +0800

    1

--- README.md
+++
@@ -1,2 +0,0 @@
-# WhyAreWeEvenDoingThis
-FEM sucks.
--- www/services/mobileApps/settings/RAFA2POC.js
+++
@@ -1,5 +0,0 @@
-﻿var RAFA2POCClient;
-document.addEventListener("deviceready", function () {
-    RAFA2POCClient = new WindowsAzure.MobileServiceClient(
-                    "https://rafa2poc.azurewebsites.net");
-});
